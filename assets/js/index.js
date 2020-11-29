141b8e1832d6d81c45737eab9a4ac38f6e43960d
Lukas Mark Jakobsen <lukasjakobsen17@hotmail.com>
2020-11-29 21:54:54 +0100

    Forside + navigation styling

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+let hoverNavs = document.querySelectorAll(".dropdown")
+let counter = 0
+
+
+
+
+hoverNavs.forEach((nav)=>{
+
+    nav.addEventListener("mouseover", function(e){
+
+
+        
+    })
+})
